beab8137cae304f5b225618896ab69e5e7f11d7c
LauroRambo <lauroorambo@gmail.com>
2023-10-10 15:48:18 -0300

    feat: add initial components for auth page

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -32,15 +32,6 @@ export const InputWrapper = styled.div`
 
 export const Input = styled.input<InputProps>`
     ${({ theme }) => css`
-        font-family: ${theme.font.family};
-        font-style: normal;
-        font-weight: ${theme.font.bold};
-        font-size: 16px;
-        line-height: 25px;
-
-        display: flex;
-        align-items: center;
-
-        color: ${theme.colors.black};
+        
     `}
 `
