2a7c19dd36c8ca26d1baaa43df817abc2d3ea36b
LauroRambo <lauroorambo@gmail.com>
2023-10-27 17:14:08 -0300

    refactor: change style of auth layout and general

--- src/components/Layouts/AuthLayout/index.tsx
+++ src/components/Layouts/AuthLayout/index.tsx
@@ -1,12 +1,17 @@
+import Input from '@/components/Input'
+import * as S from './styles'
+import Button from '@/components/Button'
+
 type AuthLayoutProps = {
     children: React.ReactNode
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
     return (
-        <>
+        <S.Container>
             {children}
-        </>
+        </S.Container>
+            
     )
 }
 
