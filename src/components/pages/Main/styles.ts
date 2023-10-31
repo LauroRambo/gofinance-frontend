88d13e720c72370a5cd0625a1ff1a517fb1f63cc
LauroRambo <lauroorambo@gmail.com>
2023-10-31 17:04:32 -0300

    feat: add graph style to main

--- src/components/pages/Main/styles.ts
+++ src/components/pages/Main/styles.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.main`
 
 export const Main = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
@@ -19,7 +19,8 @@ export const Graph = styled.div`
 export const Cards = styled.div`
   display: flex;
   flex-direction: column;
-  
+  justify-content: center;
+  align-items: center;
 `
 
 
