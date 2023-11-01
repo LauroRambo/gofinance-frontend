3afbb30e7e77cf859fd92c1c9963e3d5504f2964
LauroRambo <lauroorambo@gmail.com>
2023-11-01 17:24:37 -0300

    feat: add new accounts debit page

---
+++ src/pages/accounts-debit.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import AccountsDebitComponent from '@/components/pages/AccountsDebit'
+
+
+export default function AccountsDebit() {
+    return (
+        <DefaultLayout>
+            <AccountsDebitComponent />
+        </DefaultLayout>
+    )
+}
