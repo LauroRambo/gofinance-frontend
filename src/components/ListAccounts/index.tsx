23d80ecdb9bfa9c391be12cb15d6326e7ba59163
LauroRambo <lauroorambo@gmail.com>
2023-11-07 17:09:55 -0300

    refactor: change style and add new components in list accounts

--- src/components/ListAccounts/index.tsx
+++ src/components/ListAccounts/index.tsx
@@ -9,6 +9,7 @@ import React from 'react'
 import * as S from './styles'
 import Input from '../Input'
 import Button from '../Button'
+import { Bold } from 'styled-icons/boxicons-regular'
 
 
 
@@ -36,51 +37,54 @@ export default function ListAccounts() {
                     Adicionar
                 </Button>
             </S.Button>
-            <S.Filters>
-                <Input label="Data Inicial" type="date" name="initialDate" isFilter/>
-                <S.Filter>
-                    <Input label="Data Final" type="date" name="finalDate" isFilter />
-                </S.Filter>
-                <S.Filter>
-                    <Input label="Título" type="text" placeholder='Título' name="title" isFilter/>
-                </S.Filter>
-                <S.Filter>
-                    <Input label="Descrição" type="text" placeholder='Descrição' name="description" isFilter/>
-                </S.Filter>
-                <S.Filter>
-                    <Input label="Categoria" type="text" placeholder='Categoria' name="category" isFilter/>
-                </S.Filter>
-            </S.Filters>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="list accounts">
-                    <TableHead>
-                        <TableRow>
-                            {titles.map((title) => (
-                                <TableCell>{title}</TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.title}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.title}
-                                </TableCell>
-                                <TableCell align="right">{row.description}</TableCell>
-                                <TableCell align="right">{row.category}</TableCell>
-                                <TableCell align="right">{row.value}</TableCell>
-                                <TableCell align="right">{row.date}</TableCell>
-                                <TableCell align="right">
-                                    <S.IconDelete size={20} onClick={() => console.log('delete')} />
-                                </TableCell>
+            <S.FiltersAndDataTable>
+                <S.Filters>
+                    <Input label="Data Inicial" type="date" name="initialDate" isFilter />
+                    <S.Filter>
+                        <Input label="Data Final" type="date" name="finalDate" isFilter />
+                    </S.Filter>
+                    <S.Filter>
+                        <Input label="Título" type="text" placeholder='Título' name="title" isFilter />
+                    </S.Filter>
+                    <S.Filter>
+                        <Input label="Descrição" type="text" placeholder='Descrição' name="description" isFilter />
+                    </S.Filter>
+                    <S.Filter>
+                        <Input label="Categoria" type="text" placeholder='Categoria' name="category" isFilter />
+                    </S.Filter>
+                </S.Filters>
+                <TableContainer component={Paper} sx={{ mt:2 }}>
+                    <Table sx={{ minWidth: 650, fontSize: 60 }} aria-label="list accounts">
+                        <TableHead>
+                            <TableRow>
+                                {titles.map((title) => (
+                                    <TableCell sx={{ fontSize: 15, fontWeight: 'bold' }}>{title}</TableCell>
+                                ))}
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.title}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row" sx={{ fontSize: 15 }}>
+                                        {row.title}
+                                    </TableCell>
+                                    <TableCell align="left" sx={{ fontSize: 15 }}>{row.description}</TableCell>
+                                    <TableCell align="left" sx={{ fontSize: 15 }}>{row.category}</TableCell>
+                                    <TableCell align="left" sx={{ fontSize: 15 }}>{row.value}</TableCell>
+                                    <TableCell align="left" sx={{ fontSize: 15 }}>{row.date}</TableCell>
+                                    <TableCell align="left" sx={{ fontSize: 20 }}>
+                                        <S.IconDelete size={20} onClick={() => console.log('delete')} />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </S.FiltersAndDataTable>
+
         </S.Wrapper>
     )
 }
--- src/components/ListAccounts/styles.ts
+++ src/components/ListAccounts/styles.ts
@@ -5,28 +5,35 @@ import { AiOutlineDelete } from 'react-icons/ai'
 export const Wrapper = styled.main`
     display: flex;
     flex-direction: column;
+`
 
+export const FiltersAndDataTable = styled.div`
+    ${({ theme }) => css`
+        background: ${theme.colors.white};
+        padding: 15px;
+        border-radius: 7px;
+        box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2);    
+    `}
 `
 
 export const Filters = styled.div`
     display: flex;
     flex-direction: row;
-    
 `
+
 export const Button = styled.div`
     display: flex;
     justify-content: flex-end;
     align-items: flex-end;
-    
 `
 
 export const Filter = styled.div`
-    margin-left: 5px;
+    margin-left: 35px;
     
 `
 
 export const IconDelete = styled(AiOutlineDelete)`
-    ${ ({ theme }) => css`
+    ${({ theme }) => css`
         color: ${theme.colors.tomato};    
     `}
     cursor: pointer;
